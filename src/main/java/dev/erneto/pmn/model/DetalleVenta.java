ea5126cfc6f4074470b3ae12d26e3e3042024df5
erneto13 <erneto.amaral13@gmail.com>
2026-07-04 03:50:39 -0700

    Agregar anotaciones de Lombok en DetalleVenta para mejorar la legibilidad y facilitar la creación de instancias

--- src/main/java/dev/erneto/pmn/model/DetalleVenta.java
+++ src/main/java/dev/erneto/pmn/model/DetalleVenta.java
@@ -1,13 +1,19 @@
 package dev.erneto.pmn.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Entity
 @Table(name = "detalles_venta")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DetalleVenta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,7 +31,7 @@ public class DetalleVenta {
     private int cantidad;
 
     @Column(nullable = false, precision = 12, scale = 2)
-    private BigDecimal precio_unitario;
+    private BigDecimal precioUnitario;
 
     @Column(nullable = false, precision = 14, scale = 2)
     private BigDecimal subtotal;
